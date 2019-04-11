10d23712670fd0ecca166ffc393d669967818a5f
andre_viallon <viallon.andre@gmail.com>
2019-04-11 17:54:22 +0200

    Added a Decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,9 +4,18 @@ class Counter extends Component {
     render() {
         return (
             <React.Fragment>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm ">Increment</button>
-                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+                <br />
+                <div className="row">
+                    <div className="col-1">
+                        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                    </div>
+                    <div className="col">
+                        <button onClick={() => this.props.onIncrement(this.props.counter)} className={this.getSecondaryBtnClasses()}>+</button>
+                        <button onClick={() => this.props.onDecrement(this.props.counter)} disabled={this.props.counter.value === 0} className={this.getSecondaryBtnClasses()}>-</button>
+                        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-1 delete-btn">X</button>
+                    </div>
+
+                </div>
             </React.Fragment>
         );
     }
@@ -14,6 +23,11 @@ class Counter extends Component {
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
         classes += (this.props.counter.value === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    getSecondaryBtnClasses() {
+        let classes = 'btn btn-secondary btn-sm m-1';
         return classes;
     }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -7,7 +7,7 @@ class Counters extends Component {
       <div>
         <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
         {this.props.counters.map(counter => (
-          <Counter key={counter.id} counter={counter} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} />
+          <Counter key={counter.id} counter={counter} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} onDecrement={this.props.onDecrement} />
         ))}
       </div>
     );
